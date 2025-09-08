8c83b17213b07602cd611f7265324fc11fd6b731
Vinay Vastrakar <vinay.git.repo@gmail.com>
2025-09-08 09:05:53 +0530

    add actuator in security config

--- Asset-Management-Application/src/main/java/com/example/Assets/Management/App/security/SecurityConfig.java
+++ Asset-Management-Application/src/main/java/com/example/Assets/Management/App/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configure(http))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/refresh-token","/api/auth/**","/api/auth/google-login","/swagger-ui/**","/swagger-ui.html","/v3/api-docs/**").permitAll()
+                .requestMatchers("/api/auth/refresh-token","/api/auth/**","/api/auth/google-login","/swagger-ui/**","/swagger-ui.html","/v3/api-docs/**","/actuator/**").permitAll()
                 .requestMatchers(HttpMethod.GET).hasAnyRole("ADMIN", "USER")
                 // .requestMatchers("/api/categories/**").hasAnyRole("ADMIN", "USER")
                 .requestMatchers("/api/**").hasRole("ADMIN")
